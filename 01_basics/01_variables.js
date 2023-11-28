efa611ced94580cf00ecd8a4f1b7ad8d6edbf179
Praash0724 <147259820+Praash0724@users.noreply.github.com>
2023-11-28 07:13:45 +0000

    conatiner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =14453
+let accountEmail="prabal@google.com"
+var accountPassword="12345"
+accountCity="Bangalore"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountcity="Jaipur"
+
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
